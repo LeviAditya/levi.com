a59cc7e688e089e5a46cd219649c19eba8fea2f8
LeviAditya <160210244+LeviAditya@users.noreply.github.com>
2024-02-16 13:37:49 +0700

    Add files via upload

---
+++ lep.js
@@ -0,0 +1,11 @@
+function test() {
+    var input = prompt("Zeta Itu Siapa?");
+    var show = document.getElementById("show")
+
+    if (input === "punya lepi") {
+        show.textContent = "sip thankyou lope yu"
+    }
+    else {
+        show.textContent = "goblok lu gay"
+    }
+}
